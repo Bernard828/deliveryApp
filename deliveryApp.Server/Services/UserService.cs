775b698d52b6cc02c0744928a0ee77a891f50671
Bernard828 <bernard.lackey1@gmail.com>
2026-06-06 16:37:24 -0400

    UserService added.

---
+++ deliveryApp.Server/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using deliveryApp.Server.Data;
+using deliveryApp.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace deliveryApp.Server.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User> CreateUserAsync(User user);
+        Task<bool> UpdateUserAsync(int id, User user);
+        Task<bool> DeleteUserAsync(int id);
+    }
+    public class UserService : IUserService
+    {
+        private readonly DeliveryAppDbContext _context;
+
+        public UserService(DeliveryAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.Include(u => u.Role).ToListAsync();
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+           return await _context.Users
+            .Include(u => u.Role)
+            .FirstOrDefaultAsync(u => u.UserId == id);
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync()
+                return user;
+        }
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
