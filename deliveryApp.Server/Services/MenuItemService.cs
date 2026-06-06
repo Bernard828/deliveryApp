2d6fc14525638914628004a9515f329cde8e1f96
Bernard828 <bernard.lackey1@gmail.com>
2026-06-06 15:55:07 -0400

    IMenuItemService created.

---
+++ deliveryApp.Server/Services/MenuItemService.cs
@@ -0,0 +1,16 @@
+﻿using deliveryApp.Server.Models;
+
+namespace deliveryApp.Server.Services
+{
+    public interface IMenuItemService
+    {
+        Task<IEnumerable<MenuItem>> GetMenuByRestaurantIdAsync(int restaurantId);
+        Task<MenuItem> GetByIdAsync(int id);
+        Task<MenuItem> Createasync(MenuItem item);
+        Task<bool> UpdateAsync(int id, MenuItem item);
+        Task<bool> DeleteAsync(int id);
+    }
+    public class MenuItemService
+    {
+    }
+}
