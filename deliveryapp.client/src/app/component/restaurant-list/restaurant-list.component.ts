a966f64aa2e2c7ac2d82fa6fd69e07e55462ecde
Bernard828 <bernard.lackey1@gmail.com>
2026-07-12 21:21:30 -0400

    Angular upgrade v20 -> v21.

--- deliveryapp.client/src/app/component/restaurant-list/restaurant-list.component.ts
+++ deliveryapp.client/src/app/component/restaurant-list/restaurant-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RestaurantService } from "../../services/restaurant.service";
 import { RestaurantSearchDto,RestaurantCreateDto} from '../../models/restuarant.model';
