ebe5a10c7c5c9e9efc88e331a0e9b43f0eb44154
Bernard828 <bernard.lackey1@gmail.com>
2026-07-12 21:57:21 -0400

    Imported PrimeNG

--- deliveryapp.client/src/app/app.module.ts
+++ deliveryapp.client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantListComponent } from './component/restaurant-list/restaurant-list.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { OrderTrackingComponent } from './component/order-tracking/order-tracking.component';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,18 @@ import { OrderTrackingComponent } from './component/order-tracking/order-trackin
     BrowserModule, HttpClientModule,
     AppRoutingModule, AppComponent
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkmodeselector: '[data-theme="dark"]',
+          lightmodeselector: '[data-theme="light"]'
+        }
+      }
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
