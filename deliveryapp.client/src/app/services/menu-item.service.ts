47998b6ce2412d78210b720eaf1a20fc44c93f55
Bernard828 <bernard.lackey1@gmail.com>
2026-07-13 23:22:28 -0400

    MenuItem service added, includes get full restaurant menu and get menu item bu id.

--- deliveryapp.client/src/app/services/menu-item.service.ts
+++ deliveryapp.client/src/app/services/menu-item.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../app/environment/environment';
+import { MenuItemSearchDto } from '../models/menuItem.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuItemService {
-
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUri + 'menuItems';
   constructor() { }
+
+  getByRestaurantId(restaurantId: number) {
+    return this.http.get<MenuItemSearchDto[]>(`${this.apiUrl}/restaurant/${restaurantId}`);
+  }
+
+  getById(id: number) {
+    return this.http.get<MenuItemSearchDto>(`${this.apiUrl}/${id}`);
+  }
 }
