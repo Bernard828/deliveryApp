7367db6b315aa759c48e0b087b6338ab7b4b5853
Bernard828 <bernard.lackey1@gmail.com>
2026-07-13 20:09:14 -0400

    Code-cleanup to app.component for modern Angular practice. Removed unnecessary httpclient import.

--- deliveryapp.client/src/app/app.component.ts
+++ deliveryapp.client/src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, CommonModule],
-  changeDetection: ChangeDetectionStrategy.Eager,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent{
-  constructor(private http: HttpClient) { }
+export class AppComponent {
   title = 'Food Delivery App';
 
 }
--- deliveryapp.client/src/app/app.module.ts
+++ deliveryapp.client/src/app/app.module.ts
@@ -1,45 +1,45 @@
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { NgModule, ApplicationConfig } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { AppRoutingModule, routes } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RestaurantListComponent } from './component/restaurant-list/restaurant-list.component';
-import { MenuComponent } from './component/menu/menu.component';
-import { OrderTrackingComponent } from './component/order-tracking/order-tracking.component';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+// import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+// import { NgModule, ApplicationConfig } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { provideRouter } from '@angular/router';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { AppRoutingModule, routes } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { RestaurantListComponent } from './component/restaurant-list/restaurant-list.component';
+// import { MenuComponent } from './component/menu/menu.component';
+// import { OrderTrackingComponent } from './component/order-tracking/order-tracking.component';
+// import { providePrimeNG } from 'primeng/config';
+// import Aura from '@primeuix/themes/aura';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-};
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes),
+//     provideHttpClient()
+//   ]
+// };
 
-@NgModule({
-  declarations: [
-    MenuComponent,
-    OrderTrackingComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule, AppComponent
-  ],
-  providers: [
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
-          darkmodeselector: '[data-theme="dark"]',
-          lightmodeselector: '[data-theme="light"]'
-        }
-      }
-    })
-  ],
-  bootstrap: []
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [
+//     MenuComponent,
+//     OrderTrackingComponent
+//   ],
+//   imports: [
+//     BrowserModule, HttpClientModule,
+//     AppRoutingModule, AppComponent
+//   ],
+//   providers: [
+//     provideAnimationsAsync(),
+//     providePrimeNG({
+//       theme: {
+//         preset: Aura,
+//         options: {
+//           darkmodeselector: '[data-theme="dark"]',
+//           lightmodeselector: '[data-theme="light"]'
+//         }
+//       }
+//     })
+//   ],
+//   bootstrap: []
+// })
+// export class AppModule { }
 
