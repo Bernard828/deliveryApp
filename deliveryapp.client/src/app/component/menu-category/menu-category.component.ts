eef82934808aecad6393e7f5bea12ac25192b61c
Bernard828 <bernard.lackey1@gmail.com>
2026-07-14 00:16:45 -0400

    Menu category added.

--- deliveryapp.client/src/app/component/menu-category/menu-category.component.ts
+++ deliveryapp.client/src/app/component/menu-category/menu-category.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuItemSearchDto } from '../../models/menuItem.model';
+import { MenuItemCardComponent } from '../menu-item-card/menu-item-card.component';
 
 @Component({
   selector: 'app-menu-category',
-  standalone: false,
+  standalone: true,
+  imports:[CommonModule,MenuItemCardComponent],
   templateUrl: './menu-category.component.html',
-  styleUrl: './menu-category.component.css',
+  styleUrls: ['./menu-category.component.css'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class MenuCategoryComponent {
-
+  @Input() title!: string;
+  @Input() items: MenuItemSearchDto[] = [];
 }
