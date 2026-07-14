69f2f0dea8b9bd5d89f246e805f39ebbec85e9a0
Bernard828 <bernard.lackey1@gmail.com>
2026-07-13 21:18:41 -0400

    Deleteing app.module.ts for a more modern Angular coding practice.

--- deliveryapp.client/src/app/app.routes.ts
+++ deliveryapp.client/src/app/app.routes.ts
@@ -3,31 +3,41 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./component/home/home.component').then(m => m.HomeComponent)
+    loadComponent: () =>
+      import('./component/home/home.component').then(m => m.HomeComponent)
   },
   {
     path: 'restaurants',
-    loadComponent: () => import('./component/restaurant-list/restaurant-list.component').then(m => m.RestaurantListComponent)
+    loadComponent: () =>
+      import('./component/restaurant-list/restaurant-list.component').then(m => m.RestaurantListComponent)
   },
   {
     path: 'menu',
-    loadComponent: () => import('./component/menu/menu.component').then(m => m.MenuComponent)
+    loadComponent: () =>
+      import('./component/menu/menu.component').then(m => m.MenuComponent)
   },
   {
     path: 'order',
-    loadComponent: () => import('./component/order/order.component').then(m => m.OrderComponent)
+    loadComponent: () =>
+      import('./component/order/order.component').then(m => m.OrderComponent)
   },
   {
     path: 'track',
-    loadComponent: () => import('./component/order-tracking/order-tracking.component').then(m => m.OrderTrackingComponent)
+    loadComponent: () =>
+      import('./component/order-tracking/order-tracking.component').then(m => m.OrderTrackingComponent)
   },
   {
     path: 'user',
-    loadComponent: () => import('./component/user/user.component').then(m => m.UserComponent)
+    loadComponent: () =>
+      import('./component/user/user.component').then(m => m.UserComponent)
   },
   {
     path: '',
-    redirectTo: '/restaurants', pathMatch: 'full'
+    loadComponent: () =>
+      import('./component/home/home.component').then(m => m.HomeComponent)
   },
-  { path: '**', redirectTo: '' }
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
