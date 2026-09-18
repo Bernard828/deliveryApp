fb54e976e501ae331da9e5803782940c3f42cc40
Bernard Lackey Jr. <bernard.lackey1@gmail.com>
2026-09-18 19:25:05 -0400

    OperatingHoursController initalized

---
+++ deliveryApp.Server/Controllers/OperatingHoursController.cs
@@ -0,0 +1,35 @@
+﻿using deliveryApp.Server.DTOs;
+using deliveryApp.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace deliveryApp.Server.Controllers
+{
+    public class OperatingHoursController:BaseApiController
+    {
+        private readonly IOperatingHoursService _service;
+        public OperatingHoursController(OperatingHoursService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OperatingHourDto>>> GetByRestaurant(int restaurantId)
+        {
+            return Ok(await _service.GetByRestaurantAsync(restaurantId))
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, OperatingHourUpdateDto dto)
+        {
+            var update = await _service.UpdateAsync(id, dto);
+            return update ? NoContent() : NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _service.DeleteAsync(id);
+            return deleted ? NoContent() : NotFound();
+        }
+    }
+}
