e98f71de0fc57a85e17a49d8af3dc56a55f698f7
Bernard Lackey Jr. <bernard.lackey1@gmail.com>
2026-09-17 22:11:38 -0400

    CuisineTypeController initialized

--- deliveryApp.Server/Controllers/CuisineTypeController.cs
+++ deliveryApp.Server/Controllers/CuisineTypeController.cs
@@ -1,44 +1,54 @@
-﻿//using deliveryApp.Server.Models;
-//using deliveryApp.Server.Services;
-//using Microsoft.AspNetCore.Mvc;
+﻿using deliveryApp.Server.DTOs;
+using deliveryApp.Server.Models;
+using deliveryApp.Server.Services;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace deliveryApp.Server.Controllers
-//{
-//    public class CuisineTypeController : BaseApiController
-//    {
-//        private readonly ICuisineTypeService _service;
-//        public CuisineTypeController(ICuisineTypeService service)
-//        {
-//            _service = service;
-//        }
+namespace deliveryApp.Server.Controllers
+{
+    public class CuisineTypeController : BaseApiController
+    {
+        private readonly ICuisineTypeService _service;
+        public CuisineTypeController(ICuisineTypeService service)
+        {
+            _service = service;
+        }
 
-//        [HttpPost]
-//        public async Task<IActionResult> Create(CuisineTypeCreateDto dto)
-//        {
-//            var result = await _service.CreateAsync(dto);
-//            return Ok(result);
-//        }
+        [HttpPost]
+        public async Task<ActionResult<CuisineTypeDto>> Create(CuisineTypeCreateDto dto)
+        {
+            var result = await _service.CreateAsync(dto);
+            return Ok(result);
+        }
 
-//        [HttpPut]
-//        public async Task<IActionResult> Update(int id, CuisineTypeUpdateDto dto)
-//        {
-//            if (id != dto.CuisineTypeId) return BadRequest("Id mismatch.");
-//            var updated = await _service.UpdateAsync(dto);
-//            if (!updated) return NotFound();
-//            return Ok(updated);
-//        }
+        [HttpPut]
+        public async Task<ActionResult> Update(int id, CuisineTypeUpdateDto dto)
+        {
+            if (id != dto.CuisineTypeId) return BadRequest("Id mismatch.");
+            var updated = await _service.UpdateAsync(id, dto);
+            if (!updated) return NotFound();
+            return Ok(updated);
+        }
 
-//        [HttpGet]
-//        public async Task<ActionResult<IEnumerable<CuisineType>>> GetAll()
-//        {
-//            try
-//            {
-//            var result = await _service.GetAllAsync();
-//            return Ok(result);
-//            }catch(Exception ex)
-//            {
-//                return StatusCode(500, $"Internal server error: {ex.Message}");
-//            }
-//        }
-//    }
-//}
+        [HttpGet]
+        public async Task<ActionResult<List<CuisineTypeDto>>> GetAll()
+        {
+            try
+            {
+                var result = await _service.GetAllAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CuisineTypeDto>> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+    }
+}
