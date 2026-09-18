c373a1cd1eab29cc200fec59c3e1b37922c97655
Bernard Lackey Jr. <bernard.lackey1@gmail.com>
2026-09-18 19:19:09 -0400

    AddressController initialized

---
+++ deliveryApp.Server/Controllers/AdressesController.cs
@@ -0,0 +1,36 @@
+﻿using deliveryApp.Server.DTOs;
+using deliveryApp.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace deliveryApp.Server.Controllers
+{
+    public class AdressesController : BaseApiController
+    {
+        private readonly IAddressService _service;
+        public AdressesController(IAddressService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AddressDto>> Create([FromQuery] int restaurantId, AddressCreateDto dto)
+        {
+            var result = await _service.CreateAsync(restaurantId, dto);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(int restaurantId, AddressUpdateDto dto)
+        {
+            var updated = await _service.UpdateAsync(restaurantId, dto);
+            return updated ? NoContent() : NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int restaurantId)
+        {
+            var deleted = await _service.DeleteAsync(restaurantId);
+            return deleted ? NoContent() : NotFound();
+        }
+    }
+}
