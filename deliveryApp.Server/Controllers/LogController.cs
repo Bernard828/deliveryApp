118503bb02ad961a46282d172a3f8ca90da4bb34
Bernard Lackey Jr. <bernard.lackey1@gmail.com>
2026-09-21 17:41:54 -0400

    API Log model and controller created.

---
+++ deliveryApp.Server/Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using deliveryApp.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace deliveryApp.Server.Controllers
+{
+    public class LogController : BaseApiController
+    {
+        private readonly ILogger<LogController> _logger;
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult PostLog([FromBody] LogModel model)
+        {
+            switch (model.Level?.ToUpper())
+            {
+                case "DEBUG": _logger.LogDebug(model.Message); break;
+                case "WARN": _logger.LogWarning(model.Message); break;
+                case "ERROR": _logger.LogError(model.Message); break;
+                default: _logger.LogInformation(model.Message); break;
+            }return Ok();
+        }        
+    }
+}
---
+++ deliveryApp.Server/Models/LogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace deliveryApp.Server.Models
+{
+    public class LogModel
+    {
+        public string? Level { get; set; }
+        public string? Message { get; set; }
+    }
+}
